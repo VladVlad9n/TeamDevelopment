b5d723ab825bcc5862fa3a6f1cbd117ed628d5d1
VladVlad9n <46713992+VladVlad9n@users.noreply.github.com>
2019-11-29 21:28:35 +0500

    Add files via upload

---
+++ TeamDevelopment/TeamDevelopment/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace TeamDevelopment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label6 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label9 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(112, 466);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Старт";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(109, 419);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Введите время в секундах:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(112, 435);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(213, 25);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "0";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(185, 466);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(67, 25);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Стоп";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(258, 466);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(67, 25);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Сброс";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(203, 358);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 37);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "-";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(185, 466);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(67, 25);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Продолжить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TeamDevelopment.Properties.Resources._6;
+            this.pictureBox1.Location = new System.Drawing.Point(57, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(327, 288);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 16;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label9.Location = new System.Drawing.Point(332, 358);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 37);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "0";
+            this.label9.Visible = false;
+            this.label9.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 529);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label9;
+    }
+}
---
+++ TeamDevelopment/TeamDevelopment/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace TeamDevelopment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            button4.Visible = false;
+        }
+
+        public int h, m,m1, s;
+        double d,d1,d2,d3,d4,d5,d6;
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button4.Visible = true;
+            timer1.Stop();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            label6.Text = "0";
+            textBox3.Text = "";
+            pictureBox1.Image = Properties.Resources._6;
+            button4.Visible = false;
+            button1.Visible = true;
+
+            button2.Width = 67;
+            button2.BackColor = Control.DefaultBackColor;
+            button2.Location = new Point(185, 466);
+
+            button4.Width = 67;
+            button4.BackColor = Control.DefaultBackColor;
+            button4.Location = new Point(185, 466);
+            button1.Visible = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            button4.Visible = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+                s = s - 1;
+                if (s == -1)
+                {
+                    m = m - 1;
+                    s = 59;
+                }
+
+            if (m == -1)
+            {
+                h = h - 1;
+                m = 59;
+            }
+
+            if (h == 0 && m == 0 && s == 0)
+            {
+                timer1.Stop();
+                
+            }
+
+            d = (Convert.ToDouble(textBox3.Text) / 5);
+            d1 = Convert.ToDouble(textBox3.Text);
+            
+            d2 = d1 - d;
+            d3 = d2 - d;
+            d4 = d3 - d;
+            d5 = d4 - d;
+            d6 = d5 - d;
+            if (s == Convert.ToInt32(d2)) 
+            {
+                pictureBox1.Image = Properties.Resources._2;
+            }
+            label9.Text = Convert.ToString(d2);
+            if (s == Convert.ToInt32(d3))
+            {
+                pictureBox1.Image = Properties.Resources._3;
+            }
+            if (s == Convert.ToInt32(d4))
+            {
+                pictureBox1.Image = Properties.Resources._4;
+            }
+            if (s == Convert.ToInt32(d5))
+            {
+                pictureBox1.Image = Properties.Resources._5;
+            }
+            if (s == Convert.ToInt32(d6)/10)
+            {
+                pictureBox1.Image = Properties.Resources._6;
+                MessageBox.Show("Время вышло!");
+
+
+                button2.Width = 67;
+                button2.BackColor = Control.DefaultBackColor;
+                button2.Location = new Point(185, 466);
+
+                button4.Width = 67;
+                button4.BackColor = Control.DefaultBackColor;
+                button4.Location = new Point(185, 466);
+            }
+            
+            label6.Text = Convert.ToString(s);
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            s = Convert.ToInt32(textBox3.Text);
+
+            timer1.Start();
+            pictureBox1.Image = Properties.Resources._1;
+            button1.Visible = false;
+            button2.Width = 140;
+            button2.BackColor = Color.Red;
+            button2.Location = new Point(112,466);
+
+            button4.Width = 140;
+            button4.BackColor = Color.Blue;
+            button4.Location = new Point(112, 466);
+        }
+    }
+}
